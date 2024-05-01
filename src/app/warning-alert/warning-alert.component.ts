9d073c86b258763467dd38ecf22b71af42f81fce
Kamilo <kamil.kazmierczak@pm.me>
2024-05-01 15:23:55 +0200

    Exercise 1: Internal and external styling of components, creating new components manually and with cli

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styles: `p { color: red }` // some example of inline styling
+})
+export class WarningAlertComponent {
+
+}
